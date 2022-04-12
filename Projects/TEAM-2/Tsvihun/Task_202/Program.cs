90b8a19aeac382f8c41f194c3d4092a9989e3712
LoveMyDaughter <tsav1986@gmail.com>
2022-04-12 16:53:52 +0300

    Add tasks 201-205

---
+++ Projects/TEAM-2/Tsvihun/Task_202/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+202.
+Напишіть програму, яка переводить гривні в $, Є.
+*/
+using System;
+
+public class CurrencyExchange
+{
+    public static void Main(string[] args)
+    {
+        double usd_rate = 29.4;
+        double eur_rate = 32.06;
+
+        Console.WriteLine("Enter amount: ");
+        double uah = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{uah} grn = {uah / usd_rate} dollars");
+        Console.WriteLine($"{uah} grn = {uah / eur_rate} euros");
+    }
+}
---
+++ Projects/TEAM-2/Tsvihun/Task_205/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+205.
+Введіть три числа і виведіть на екран значення суми і добуток цих чисел.
+ */
+using System;
+
+public class Sum_Multiply
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number 1: ");
+        var num1_str = Console.ReadLine();
+        Console.WriteLine("Enter number 2: ");
+        var num2_str = Console.ReadLine();
+        Console.WriteLine("Enter number 3: ");
+        var num3_str = Console.ReadLine();
+
+
+        try
+        {
+            int num1 = Convert.ToInt32(num1_str);
+            int num2 = Convert.ToInt32(num2_str);
+            int num3 = Convert.ToInt32(num3_str);
+            Console.WriteLine($"Summ of these 3 numbers = {num1 + num2 + num3}");
+            Console.WriteLine($"Product of these 3 numbers = {num1 * num2 * num3}");
+        }
+        catch
+        {
+            Console.WriteLine("Not all are integer numbers !");
+        }
+
+
+    }
+}
