5f25f9f5940fb682c92eb54a0100c03b6d0973f4
Maksym <makcim2004leb@gmail.com>
2022-04-13 23:07:27 +0300

    add tasks 201-208

---
+++ Projects/TEAM-1/Lebid Maksym/Task202/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишіть програму, яка переводить гривні в $, Є.
+            float sum, dol, euro;
+            Console.WriteLine("Enter amount of hryvnia: ");
+            sum = Convert.ToSingle(Console.ReadLine());
+            dol = sum * 29.39f;
+            euro = sum * 32f;
+            Console.WriteLine($"Dollar: {dol}; euro: {euro}");
+        }
+    }
+}
