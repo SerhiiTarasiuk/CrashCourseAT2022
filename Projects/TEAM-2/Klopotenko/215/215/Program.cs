843dcff218073fe3676eddfea214f6d2497b1973
Vlad <vladklopotenko@gmail.com>
2022-04-13 21:48:26 +0200

    Add tasks 212-215

---
+++ Projects/TEAM-2/Klopotenko/215/215/Program.cs
@@ -0,0 +1,45 @@
+﻿/* 215. Написати програму, яка виводить на екран - наступну фігуру:
+"*********************                                                                                                                                                                                                        
+*                           *                                                                                                                                                                                                        
+*                           *                                                                                                                                                                                                        
+*                           *                                                                                                                                                                                                        
+*                           *                                                                                                                                                                                                        
+*********************                                                                                                                                                                                                        
+ширина і висота фігури встановлюються користувачем з клавіатури. */
+
+
+using System;
+using System.Linq;
+
+namespace _215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            int width = int.Parse(Console.ReadLine());
+            string empty = " ";
+            for(int i = 1; i <= length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Concat(Enumerable.Repeat(empty, length))); // multiplication of strings, using System.Linq;
+            for (int i = 1; i <= width; i++)
+            {
+                Console.Write("*");
+                Console.Write(string.Concat(Enumerable.Repeat(empty, length - 2)));
+                Console.WriteLine("*");
+                Console.WriteLine(string.Concat(Enumerable.Repeat(empty, length)));
+            }
+            for (int i = 1; i <= length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
