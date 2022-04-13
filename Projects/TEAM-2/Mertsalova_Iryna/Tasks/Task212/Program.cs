66286e0b46f782c49ff9083f828d68573a88111d
mertsalovaa <irynamertsalova@gmail.com>
2022-04-14 01:11:19 +0300

    add tasks

---
+++ Projects/TEAM-2/Mertsalova_Iryna/Tasks/Task212/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, res = 0;
+            var list = new List<int>();
+            Console.WriteLine(" Enter a number : ");
+            do
+            {
+                res = num % 10;
+                list.Add(res);
+                num /= 10;
+            } while (num != 0);
+
+            var unique = list.Distinct();
+            var length = unique.Count();
+            foreach (var i in unique)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"В числe {length} цифр");
+        }
+    }
+}
