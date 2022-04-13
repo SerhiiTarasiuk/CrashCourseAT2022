d1b1f90f9e96ba80b4696e902ef00871f1c789a0
MykolaKukla <135nickilay@gmail.com>
2022-04-13 21:23:51 +0300

    add file 201.cs

---
+++ Projects/TEAM-1/Mykola_Kukla/Tasks/201.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Напишіть програму, яка обчислює середнє арифметичне двох чисел.
+namespace Tasks
+{
+    class Program
+    {
+        static int Avrg(int a, int b)
+        {
+            return (a + b) / 2; 
+        }             
+        static void Main()
+        {
+
+            Console.WriteLine(Avrg(7, 3));
+
+
+
+        }
+    }
+}
