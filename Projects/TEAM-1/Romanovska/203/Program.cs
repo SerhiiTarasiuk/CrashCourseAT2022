d0dfe2d946c3d089882dcd32b417925cab0dc1bb
A125-coder <57454044+A125-coder@users.noreply.github.com>
2022-04-14 00:03:53 +0300

    add Tasks201-206

---
+++ Projects/TEAM-1/Romanovska/203/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _203
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Виведіть на екран наступний текст:
+            To be or not to be
+            \ Shakespeare \ */
+
+            Console.WriteLine("To be or not to be \n \\ Shakespeare \\");
+        }
+    }
+}
