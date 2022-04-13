b76ad155f5c90d2f1a802beb4868c9c60215c0c5
mertsalovaa <irynamertsalova@gmail.com>
2022-04-13 15:46:01 +0300

    add tasks 201-206

---
+++ Projects/TEAM-2/Mertsalova_Iryna/Tasks/Task204/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task204
+{
+    class Program
+    {
+        //Напишіть програму, яка обчислює квадрат будь-якого, введеного числа.
+        static void Main(string[] args)
+        {
+            float digit = 0;
+            Console.Write(" Enter any digit: ");
+            digit = float.Parse(Console.ReadLine());
+            Console.WriteLine("\n Your digit --> " + digit + "\n Square your digit --> " + Math.Pow(digit, 2));              
+        }
+    }
+}
---
+++ Projects/TEAM-2/Mertsalova_Iryna/Tasks/Task205/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task205
+{
+    class Program
+    {
+        //Введіть три числа і виведіть на екран значення суми і добуток цих чисел.
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 0, num3 = 0;
+            Console.Write(" Enter the first digit: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.Write(" Enter the second digit: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.Write(" Enter the third digit: ");
+            num3 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\n Sum of these digits --> " + (num1 + num2 + num3) +
+                "\n Mult of these digits --> " + (num1 * num2 * num3));
+        }
+    }
+}
