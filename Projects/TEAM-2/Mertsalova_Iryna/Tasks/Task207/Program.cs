9ac2607ba5600a5fa2449bf53930101f6bc0ed24
mertsalovaa <irynamertsalova@gmail.com>
2022-04-13 15:58:06 +0300

    add tasks

---
+++ Projects/TEAM-2/Mertsalova_Iryna/Tasks/Task207/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task207
+{
+    class Program
+    {
+        //Дано натуральне число а(a<100). Напишіть програму, що виводить на екран кількість цифр в цьому числі і суму цих цифр
+        static void Main(string[] args)
+        {
+            int digit;
+            Console.Write(" Enter any digit ( < 100 ): ");
+            digit = Int32.Parse(Console.ReadLine());
+            if (digit < 100)
+            {
+                if (digit > 9)
+                {
+                    Console.WriteLine(" Two number");
+                    Console.WriteLine(" Sum: " + digit / 10 + " + " + digit % 10 + " = "
+                        + ((digit / 10) + (digit % 10)) + "\n");
+                }
+                else
+                {
+                    Console.WriteLine(" One number");
+                    Console.WriteLine(" Sum = " + digit + "\n");
+                }
+            }
+        }
+    }
+}
