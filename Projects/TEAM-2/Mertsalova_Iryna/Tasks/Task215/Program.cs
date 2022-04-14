7210c8a88420a5ff443c1153d72de0885854775d
mertsalovaa <irynamertsalova@gmail.com>
2022-04-14 21:21:56 +0300

    add tasks

---
+++ Projects/TEAM-2/Mertsalova_Iryna/Tasks/Task215/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task215
+{
+    class Program
+    {
+        //Написати програму, яка виводить на екран - наступну фігуру:
+        // *********************                                                                                                                                                                                                        
+        // *                   *                                                                                                                                                                                                        
+        // *                   *                                                                                                                                                                                                        
+        // *                   *                                                                                                                                                                                                        
+        // *                   *                                                                                                                                                                                                        
+        // *********************                                                                                                                                                                                                        
+        //ширина і висота фігури встановлюються користувачем з клавіатури.
+        static void Main(string[] args)
+        {
+            char symb = ' ';
+            int weight = 0, height = 0;
+
+            Console.Write(" Enter a symb: ");
+            symb = char.Parse(Console.ReadLine());
+            Console.Write(" Enter a weight: ");
+            weight = Int32.Parse(Console.ReadLine());
+            Console.Write(" Enter a height: ");
+            height = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            outputWeight(symb, weight);
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write("\t* ");
+                for (int j = 0; j < weight - 2; j++)
+                {
+                    Console.Write("  ");
+                }
+                Console.WriteLine("*");
+            }            
+            outputWeight(symb, weight);
+            Console.WriteLine();
+        }
+
+        private static void outputWeight(char symb, int weight)
+        {
+            Console.Write("\t");
+            for (int i = 0; i < weight; i++)
+            {
+                Console.Write(symb + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Projects/TEAM-2/Mertsalova_Iryna/Tasks/Task216/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task216
+{
+    class Program
+    {
+        //Написати програму, яка виводить вміст масиву навпаки.
+        //Приклад: масив 23 11 6 перетворюється в 6 11 23
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("  Enter array[" + i + "] : ");
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\n  Before reverse: ");
+            printArr(arr);
+            Console.WriteLine("  After reverse: ");
+            Array.Reverse(arr);
+            printArr(arr);
+        }
+
+        private static void printArr(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write("   " + item);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
