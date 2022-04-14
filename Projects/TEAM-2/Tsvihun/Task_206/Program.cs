a28ce6f25536b843926cfbaac091162abeb43b61
LoveMyDaughter <tsav1986@gmail.com>
2022-04-15 01:06:28 +0300

    Add tasks 206-208

---
+++ Projects/TEAM-2/Tsvihun/Task_206/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+206. Напишіть програму, яка перевіряє число, введене з клавіатури на парність.
+*/
+using System;
+
+namespace Task_206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("EVEN");
+                }
+                else
+                {
+                    Console.WriteLine("ODD");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+        }
+    }
+}
