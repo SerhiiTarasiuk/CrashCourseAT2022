2c85edc59d0ff9eb8e412684b17dab9c2353059a
Maritcka <102948818+Maritcka@users.noreply.github.com>
2022-04-16 19:07:58 +0300

    Revert "edit"
    This reverts commit 3928d82b69c34fa673fa741a5d4ec9298bdf8eab.

---
+++ Projects/TEAM-3/Turbal/task3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//2. 3. Виведіть на екран наступний текст:
+//To be or not to be
+//\ Shakespeare \
+
+Console.WriteLine("To be or not to be \n \\ Shakespeare \\");
